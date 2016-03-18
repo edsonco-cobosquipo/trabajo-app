e803bda364dfff67de9257660c6c3bd242bab475
edson <edson@gmail.com>
2016-03-18 10:31:23 -0500

    pagina web

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+	<link rel="stylesheet" href="estilos3.css">
+</head>
+<body>
+<header>
+	<header>
+	
+	<h1>LUGARES ARQUEOLOGICOS DE CUSCO</h1>
+	<div id="prueba7">
+
+	
+	<h2><a href="http://www.glogle.com" target="blank">
+	buscador</a>
+		<a href="machupicchu1.jpg">logo</a>
+		<a href="Ollantaytambo2.jpg">logo</a>
+		<a href="saqsayhuaman3.jpg">logo</a>
+		<a href="pisaq4.jpg">logo</a>
+		<a href="pikillacta5.jpg">logo</a>
+		<a href="watogto6.jpg">logo</a>
+
+</h2>
+
+<h5>
+<div class="container">
+
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">inicio
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Link</a></li>
+				<li><a href="#">Search</a></li>
+			</ul>
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="ingrese url">
+				</div>
+				<button type="submit" class="btn btn-default">buscar</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index5.html">google</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">nuevo<b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li><a href="#">Separated link</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div>
+</nav>
+</div>
+<h5>
+	<div class="slider">
+	<div id="carousel-id" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-id" data-slide-to="0" class=""></li>
+			<li data-target="#carousel-id" data-slide-to="1" class=""></li>
+			<li data-target="#carousel-id" data-slide-to="2" class="active"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="item">
+			
+				<img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" alt="First slide" src="public/img/1.jpg" height="2000" width="2000">
+				<div class="container">
+					<div class="carousel-caption">
+						<h1></h1>
+						<p><code></code></p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button"></a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item">
+				<img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" alt="Second slide" src="public/img/2.jpg" height="2000" width="2000">>
+				<div class="container">
+					<div class="carousel-caption">
+						<h1></h1>
+						<p></p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+					</div>
+				</div>
+			</div>
+			<div class="item active">
+				<img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" alt="Third slide" src="public/img/3.jpg" height="2000" width="2000">>
+				<div class="container">
+					<div class="carousel-caption">
+						<h1></h1>
+						<p></p>
+						<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+					</div>
+				</div>
+			</div>
+			
+</h5>
+
+		</div>
+		<a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+		<a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+	</div>
+
+</h5>
+	
+	<img src= "machupicchu1.jpg" alt="" height="180" width="180">
+
+	
+	<img center src="Ollantaytambo2.jpg" alt="" height="180" width="180">
+	
+	<img center src="saqsayhuaman3.jpg" alt="" height="180" width="180">>
+
+
+	
+	<p class="bold" id="prueva" >
+
+Machu Picchu (del quechua sureño machu pikchu, Montaña Vieja) es el nombre contemporáneo que se da a una llaqta—antiguo poblado andino— incaica construida a mediados del siglo XV en el promontorio rocoso que une las montañas Machu Picchu y Huayna Picchu en la vertiente oriental de la cordillera Central, al sur del Perú y a 2490 msnm, altitud de su plaza principal. Su nombre original habría sido Picchu o Picho. 
+Según documentos de mediados del siglo XVI, Machu Picchu habría sido una de las residencias de descanso de Pachacútec, noveno inca del Tahuantinsuyo entre 1438 y 1470. Sin embargo, algunas de sus mejores construcciones y el evidente carácter ceremonial de la principal vía de acceso a la llaqta demostrarían que esta fue usada como santuario religioso. Ambos usos, el de palacio y el de santuario, no habrían sido incompatibles. Algunos expertos parecen haber descartado, en cambio, un supuesto carácter militar, por lo que los populares calificativos de fortaleza o ciudadela podrían haber sido superados. 
+
+</p>
+
+
+	</div>
+	<div>
+	
+		<p class="bold" id="prueva2" >
+Ollantaytambo (quechua: Ollantay Tampu) es un poblado y sitio arqueológico incaico, capital del distrito de Ollantaytambo(provincia de Urubamba), situado al sur del Perú, a unos 90 km al noroeste de la ciudad del Cuzco.
+Durante el incanato, Pachacútec conquistó la región y construyó el pueblo y un centro ceremonial. En la época de la conquistasirvió como fuerte de Manco Inca Yupanqui, líder de la resistencia inca. Es la única ciudad del incanato en el Perú que aún es habitada. En Ollantaytambo hay andenes de resistencia (para evitar deslizamientos), no agrícolas como en los demás sitios arqueológicos del Cuzco. En la actualidad es una importante atracción turística debido a sus construcciones incas y por ser uno de los puntos de partida más comunes del camino inca hacia Machu Picchu.
+
+</p>
+
+
+	</div>
+	
+	<p class="bold" id="prueva" >Sacsayhuamán (en quechua Saqsaywaman, de saqsay, lugar de saciarse, y waman, halcón, es decir, "Lugar donde se sacia el halcón") es una "fortaleza ceremonial" inca ubicada dos kilómetros al norte de la ciudad del Cuzco. Se comenzó a construir durante el gobierno de Pachacútec, en el siglo XV; sin embargo, fue Huayna Cápac quien le dio el toque final en el siglo XVI. Con el aniquilamiento de la nobleza inca desaparecieron de la memoria humana las técnicas que permitieron la construcción de esta monumental fortaleza o santuario; el cual produjo la admiración de Pizarro y sus hombres. Pedro Sancho, secretario de Pizarro, deja una primera descripción del edificio; pero la más detallada es la del Inca Garcilaso de la Vega (Cronista mestizo). La "fortaleza ceremonial" de Sacsayhuamán es con sus muros megalíticos, la mayor obra arquitectónica que realizaron los incas durante su apogeo. Desde la fortaleza se observa una singular vista panorámica de los entornos, incluyendo la ciudad del Cuzco.
+</p>
+
+
+	</div>
+	<img src="pisaq4.jpg" alt="" height="180" width="180">
+	<img src="pikillacta5.jpg" alt="" height="180" width="180">
+	<img src="qorikancha6.jpg" alt="" height="180" width="180">
+
+	<div>
+	<p class="bold" id="prueva4" >
+Písac (en quechua P'isaq) está en el distrito homónimo de la provincia de Calca,1 está ubicado a 30 kilómetros de la ciudad del Cusco,2 en el Perú. Su yacimiento arqueológico es uno de los más importantes y visitados del Valle Sagrado de los Incas. Se encuentra al este de la cordillera de Vilcabamba. Como era costumbre en la arquitectura inca, las ciudades fueron construidas sobre la base de trazos figurativos de animales. Písac arqueológico asume la forma de la perdiz de puna (Nohoprocta ornata branicti Taczanovski),3 como expresa su toponimia o denominación originaria.
+Písac está divido en dos zonas, la antigua, ubicada en la parte alta, es un sitio arqueológico incaico, y la actual, ubicada en el valle, que data de la época colonial. Su plaza principal es un lugar entretenido lleno de colorido y con diversos artículos artesanales a la venta. Este pueblo es conocido por su observatorio astronómico.
+La arquitectura de Písac también es mestiza construida sobre restos indígenas por el virrey Toledo.
+</p>
+
+
+	</div>
+	<div>
+		<p class="bold" id="prueva5" >
+	Pikillacta es considerada en la actualidad como una de las ciudades pre incas más conocidas y mejor conservadas que existen en el Perú. Se encuentraa 30 Km. al sudeste de la ciudad del Cusco, sobre los 3350 metros de altura y cubre un territorio de aproximadamente 50 Has.
+	En la época pre inka la Cuenca de Lucre fue ocupado por los Wari, logrando dominar de esta manera la zona, manteniendo supremacía desde Chelques-Paruro. Al respecto, nos indica Ravines (1982) que los Wari antes de asentarse en Pikillaqta, ocuparon primigeniamente el sector de Chelques.Al establecerse en la Cuenca de Lucre los Wari, su organización expansionista se limitó a acciones encaminadas al desarrollo social, económico, político, administrativo, concordante con el desarrollo de las formaciones culturales y locales.La importancia de los Wari radicó en la planificación urbana y gubernamental alcanzado de esta manera el alto grado de conocimiento en ingeniería, logrando lebantar obras arquitectónicas de gran envergadura.
+</p>
+
+
+	</div>
+	
+	<p class="bold" id="prueva" >El Coricancha (en quechua: Quri Kancha, ‘templo dorado’)?, originalmente Inti Kancha («Templo del sol») es el templo inca sobre el cual fue construido el Convento de Santo Domingo.Fue uno de los más famosos, venerados y respetados templos de la ciudad del Cuzco, en el Perú.
+Ruinas actuales del templo del Sol o Coricancha, conservadas dentro del templo de Santo Domingo.
+El recinto de oro; como era conocido, era un lugar sagrado donde se rendía adoración al máximo dios inca: el Inti (Sol), por lo que sólo podían entrar en ayunas, descalzos y con una carga en la espalda en señal de humildad, según lo indicaba el sacerdote mayor Willaq Umu .
+El frontis era un hermoso muro proveniente de la más fina cantería, decorado únicamente por una fina lámina de oro puro de una palma de alto, a tres metros del suelo, y un techo de paja fina y delicadamente cortada.
+En uno de los bloques de la segunda hilada se observan tres agujeros que pudieron ser utilizados para evacuar las aguas de las lluvias del patio interior, o como salida de la chicha que se ofrecía como ofrenda. 
+</p>
+
+
+	</div>
+
+
+</div>
+<h1>VIDEOS RALACIONADOS CON LOS SITIOS ARQUEOLOGICO DE CUSCO</h1>
+</div>
+<div class="container">	 	  
+  	<div class="row">
+  		<div class="col-md-4">
+  			<div class="mi-video">
+
+  			<h3>machupicchu</h3>
+  				<video autobuffer machupicchuop controls>
+  					<source src="machupicchu1.mp4">
+  				</video>
+  			</div>
+  		</div>
+  		<div class="col-md-4">
+  			<div class="mi-video">
+  			<h3>ollantaytambo</h3>
+  				<video autobuffer autoloop loop controls>
+  					<source src="ollantaytambo2.mp4">
+  				</video>
+  			</div>
+  		</div>
+  	<div class="col-md-4">
+  			<div class="mi-video">
+  			<h3>saqsayhuaman</h3>
+  				<video autobuffer autoloop loop controls>
+  					<source src="saqsayhuaman3.mp4">
+  				</video>
+  			</div>
+  		</div>
+ 	
+ </div>
+	<script src="public/js/jquery-1.12.1.js"></script>
+	<script src="public/js/bootstrap.js"></script>
+
+
+	
+</body>
+</html>
